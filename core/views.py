1ced4162b6a17afdea97ccf3e4002d99ee4a1b25
Faaiz S <faaizsultan1@gmail.com>
2025-02-09 15:54:05 +0500

    Initial Commit

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
